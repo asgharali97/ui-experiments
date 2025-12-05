8ce410d5592eb0b87f013bddbdeec5eefddeb982
asgharali97 <asgharalifs48@gmail.com>
2025-12-06 00:10:12 +0500

    tried pop card with GSAP flip animation

--- popup-card/src/App.tsx
+++ popup-card/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import Glassy from './components/Glassy'
+import PopupCard from "./components/PopupCard";
+import PopupCardGsap from "./components/PopupCardGsap";
 const App = () => {
   return (
     <>
       <div className="w-full h-screen bg-neutral-100 text-black flex justify-center items-center geist">
-        <Glassy/>
+        <div className="w-92 82  flex justify-center rouned-lg mx-4 py-4">
+          <PopupCard />
+        </div>
+        <div className="w-92 82  flex justify-center rouned-lg mx-4 py-4">
+          <PopupCardGsap />
+        </div>
       </div>
     </>
   );
--- popup-card/src/components/PopupCardGsap.tsx
+++ popup-card/src/components/PopupCardGsap.tsx
@@ -1,68 +1,65 @@
 import { IconX, IconPlus } from "@tabler/icons-react";
-import { useState } from "react";
-import { motion } from "motion/react";
+import { useState, useRef } from "react";
 import folder from "../assets/folder.svg";
 import task from "../assets/task.svg";
 import note from "../assets/note.svg";
 import goal from "../assets/goal.svg";
 import bolt from "../assets/bolt.svg";
 import calender from "../assets/calender.svg";
+import gsap from "gsap";
+import { Flip } from "gsap/Flip";
+import { flushSync } from "react-dom";
+gsap.registerPlugin(Flip);
 
-const PopupCard = () => {
+const PopupCardGsap = () => {
   const [open, setOpen] = useState(false);
+  const headRef = useRef(null);
+  const iconRef = useRef(null);
+
+  const cardRef = useRef(null);
+
+  const toggle = () => {
+    const state = Flip.getState(cardRef.current);
+
+    flushSync(() => {
+      setOpen((prev) => !prev);
+    });
+
+    Flip.from(state, {
+      duration: 0.3,
+      ease: "power2.inOut",
+    });
+  };
+
   return (
     <>
       <div>
-         <h1
-          className="text-neutral-600 text-lg font-medium text-center my-2"
-          >
-            With Motion
-          </h1>
+        <h1 className="text-neutral-600 text-lg font-medium text-center my-2">
+          With GSAP
+        </h1>
+        <div
+          ref={cardRef}
+          className={`
+          transition-all 
+          ${
+            open
+              ? "w-88 h-74 rounded-3xl"
+              : "py-2 px-6 rounded-full"
+          }
+        `}
+        >
         {open ? (
-          <motion.div
-            layoutId="card-container"
-            layout
-            initial={{
-              opacity: 0,
-              filter: "blur(10px)",
-            }}
-            animate={{
-              opacity: 1,
-              filter: "blur(0px)",
-            }}
-            transition={{
-              duration: 0.3,
-              ease: "easeInOut",
-            }}
-            className="w-88 h-74 border border-[#bbbbbb] rounded-3xl flex flex-col shadow-[0_0.2px_4px_rgb(0,0,0,0.2)]"
-          >
+          <div className="w-88 h-74 border border-[#bbbbbb] rounded-3xl flex flex-col shadow-[0_0.2px_4px_rgb(0,0,0,0.2)]">
             <div className="py-3 px-6 flex justify-between items-center rounded-t-3xl">
-              <motion.h1
-                layoutId="card-head"
-                className="text-neutral-600 text-lg font-medium"
-              >
+              <h1 className="text-neutral-600 text-lg font-medium">
                 Create New
-              </motion.h1>
-              <motion.div
-                layoutId="card-isOpen"
-                layout
-                initial={{
-                  opacity: 0,
-                  filter: "blur(10px)",
-                }}
-                animate={{
-                  opacity: 1,
-                  filter: "blur(0px)",
-                }}
-                transition={{
-                  duration: 0.3,
-                  ease: "easeInOut",
-                }}
+              </h1>
+              <div
                 className="flex justify-center items-center h-7 w-7 rounded-full bg-neutral-400"
-                onClick={() => setOpen(!open)}
+                onClick={toggle}
               >
                 <IconX className="text-white h-5 w-5" />
-              </motion.div>
+              </div>
             </div>
             <div className="flex-1 flex justify-center rounded-2xl bg-white border border-[#bcbcbc] py-10 px-6">
               <div className="grid grid-cols-3 gap-14">
@@ -104,38 +101,22 @@ const PopupCard = () => {
                 </div>
               </div>
             </div>
-          </motion.div>
+          </div>
         ) : (
-          <motion.button
-            layoutId="card-container"
-            
-            onClick={() => setOpen(!open)}
+          <button
+            onClick={toggle}
             className="flex items-center gap-2 py-2 px-6 text-lg bg-neutral-100 text-neutral-600 font-medium  border-neutral-300 rounded-full shadow-[0.3px_0.5px_2px_0px_rgb(0,0,0,0.2)] cursor-pointer"
           >
-            <motion.span layoutId="card-head">Create New</motion.span>
-            <motion.div
-              layoutId="card-isOpen"
-              layout
-              initial={{
-                opacity: 0,
-                filter: "blur(10px)",
-              }}
-              animate={{
-                opacity: 1,
-                filter: "blur(0px)",
-              }}
-              transition={{
-                duration: 0.3,
-                ease: "easeInOut",
-              }}
-            >
+            <span>Create New</span>
+            <div>
               <IconPlus className="text-neutral-600" />
-            </motion.div>
-          </motion.button>
+            </div>
+          </button>
         )}
+        </div>
       </div>
     </>
   );
 };
 
-export default PopupCard;
+export default PopupCardGsap;
