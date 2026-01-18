6474cbd6037088bcd12876b206c935634af1a7ca
asgharali97 <asgharalifs48@gmail.com>
2026-01-18 18:36:22 +0500

    removed css class & added tailwind

--- popup-card/src/components/ConfirmMDailog.tsx
+++ popup-card/src/components/ConfirmMDailog.tsx
@@ -6,7 +6,7 @@ const ConfirmMDailog = () => {
       shadow-[inset_0px_-2px_2px_-2px_oklch(92%_0.03_85),inset_0px_2px_4px_-2px_oklch(90%_0.04_80),inset_-1px_0px_1px_-2px_oklch(90%_0.04_80),inset_1px_0px_2px_-2px_oklch(85%_0.02_90)]
       ">
         <div className="flex justify-center relative">
-          <div className="bg-gradient absolute bottom-2 w-18 h-32 rounded-full opacity-80 z-20"></div>
+          <div className="bg-[oklch(83.7% 0.128 66.29)] absolute bottom-2 w-18 h-32 rounded-full opacity-80 z-20 blur-[55px] shadow-[0_0 40px_20px_oklch(83.907%_0.11653_68.733),_0_0_100px_50px_oklch(92.856%_0.05275_69.823);]"></div>
         </div>
         <div className="flex flex-col h-full gap-4 justify-center items-end ">
           <h1 className="mt-12 text-center text-white text-lg z-50">
@@ -16,7 +16,7 @@ const ConfirmMDailog = () => {
             <button className="py-2 px-10 text-md font-light text-neutral-200 bg-neutral-800 rounded-2xl">
               Delete
             </button>
-            <button className="py-2 px-10 text-md font-light text-neutral-200 bg-neutral-800 rounded-2xl">
+            <button className="py-2 px-10 text-md font-light text-neutral-              200 bg-neutral-800 rounded-2xl">
               Cancel
             </button>
           </div>
