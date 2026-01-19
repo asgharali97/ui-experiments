f50139a5b89fe25c108ee479544878870b527dbf
asgharali97 <asgharalifs48@gmail.com>
2026-01-20 01:06:34 +0500

    added: motion animation

--- popup-card/src/components/ConfirmMDailog.tsx
+++ popup-card/src/components/ConfirmMDailog.tsx
@@ -31,11 +31,12 @@ const ConfirmMDailog = () => {
           <motion.div
             layoutId={'container'}
             className="w-82 h-48 bg-neutral-900 rounded-[2.2rem] px-8 overflow-hidden
-      shadow-[inset_0px_-2px_2px_-2px_oklch(92%_0.03_85),inset_0px_2px_4px_-2px_oklch(90%_0.04_80),inset_-1px_0px_1px_-2px_oklch(90%_0.04_80),inset_1px_0px_2px_-2px_oklch(85%_0.02_90)]
+      shadow-[inset_0px_-1px_2px_-2px_oklch(92%_0.03_85),inset_0px_2px_4px_-2px_oklch(90%_0.04_80),inset_-2px_0px_1px_-2px_oklch(90%_0.04_80),inset_2px_0px_1px_-2px_oklch(85%_0.02_90)]
       ">
             <div className="flex justify-center relative">
               <div className="bg-orange-300 absolute bottom-2 w-18 h-32 rounded-full opacity-85 z-20 blur-[55px] shadow-[0_0 40px_20px_oklch(83.907%_0.11653_68.733),_0_0_100px_50px_oklch(92.856%_0.05275_69.823)]"></div>
             </div>
+            <p></p>
             <div className="flex flex-col h-full gap-4 justify-center items-end ">
               <div className="flex justify-center items-center">
                 <motion.h1
