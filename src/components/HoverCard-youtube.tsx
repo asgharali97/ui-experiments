8d8fc33d71169dd18401f8e1e8efc7fc2dbb3d1b
asgharali97 <asgharalifs48@gmail.com>
2026-02-01 02:26:34 +0500

    chore: remove old isolated setups and clean repository root

--- src/components/HoverCard-youtube.tsx
+++ src/components/HoverCard-youtube.tsx
@@ -1,8 +1,46 @@
-import { Badge } from "@/components/ui/badge";
 import gsap from "gsap";
 import { useRef } from "react";
 import { FastAverageColor } from "fast-average-color";
-import { videos } from "./data/videos";
+
+const videos = [
+  {
+    id: 1,
+    title: "Shadcn Just Dropped Game-Changing Components | Update 2025",
+    channel: "Manu Arora",
+    views: "1M views",
+    time: "10 days ago",
+    duration: "19:23",
+    thumbnail:
+      "https://i9.ytimg.com/vi/c1B0uDAlc7c/hqdefault_custom_3.jpg?sqp=CIif3scG-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAZgEgcFIX8WGYDjxCMM757ucqMUA",
+    avatar:
+      "https://yt3.googleusercontent.com/n7G_or_yexSPKjDYTVLw59w0B7DUTWT3mGln3ghAoGQvFCwkd1lxeQTbCE_hV2q7ASJC3PU3dw=s160-c-k-c0x00ffffff-no-rj",
+  },
+   {
+    id: 2,
+    title: "Building Workflows with Motia",
+    channel: "Piyush Garg",
+    views: "2M views",
+    time: "2 days ago",
+    duration: "10:51",
+    thumbnail:
+      "https://i.ytimg.com/vi/f9JdhJlod0A/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA5V-kFvnmACcLTv6Eewr5kUjH8CA",
+    avatar:
+      "https://yt3.googleusercontent.com/3acddexuFlA5yKRS2--11NeqhCiik-0cntUPjk_QjlsA4ScmQUPWNmeBLweVUQjWXTCLT26lsw=s160-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: 3,
+    title: "The Secret Sauce Behind Modern AI Projects | AI SDK Library",
+    channel: "Chai aur Code",
+    views: "2.8M views",
+    time: "13 days ago",
+    duration: "8:40",
+    thumbnail:
+      "https://i.ytimg.com/vi/DDg6NtD1SsQ/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCrADQVhSL7UA1d-2QTTbk_cOypiw",
+    avatar:
+      "https://yt3.googleusercontent.com/6tLBV-DRVemxhmanuezR5HkHshX2g7Y46Rq8cysyO1V-nd2SaQ2Fi8cdgVM-n6v_8XZ5BEimxXI=s160-c-k-c0x00ffffff-no-rj",
+  },
+ 
+];
 
 
 const App = () => {
